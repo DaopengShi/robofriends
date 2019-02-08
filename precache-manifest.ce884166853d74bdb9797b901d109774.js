9a1607583f55f10224c38b10995cb24125781d8d
DaopengShi <holmessherlock734@gmail.com>
2019-02-08 14:30:40 +0800

    Updates

---
+++ precache-manifest.ce884166853d74bdb9797b901d109774.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "58d3fd18bd863aaf350b",
+    "url": "/robofriends/static/css/main.f81d37c3.chunk.css"
+  },
+  {
+    "revision": "58d3fd18bd863aaf350b",
+    "url": "/robofriends/static/js/main.58d3fd18.chunk.js"
+  },
+  {
+    "revision": "1fe2427b1bb068531ca8",
+    "url": "/robofriends/static/css/1.ae710d8c.chunk.css"
+  },
+  {
+    "revision": "1fe2427b1bb068531ca8",
+    "url": "/robofriends/static/js/1.1fe2427b.chunk.js"
+  },
+  {
+    "revision": "1446d6ec194445c3572d",
+    "url": "/robofriends/static/js/runtime~main.1446d6ec.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "b69ebbe5c90699c41967ee00d19cb397",
+    "url": "/robofriends/index.html"
+  }
+];
